bf6f21ae53086d42dba6a13e3949bc1cc38be37e
Bartłomiej Bartoszewicz <bartlomiej.bartoszewicz95@gmail.com>
2022-01-12 17:08:12 +0100

    Added more test cases

--- test/sql.test.js
+++ test/sql.test.js
@@ -395,11 +395,76 @@ describe('Numbers array tests', () => {
   });
 });
 
-describe('Frequency tests', () => {});
+describe('Frequency tests', () => {
+  const persons = [
+    ['Peter', 3],
+    ['Anna', 4],
+    ['Peter', 7],
+    ['Michael', 10]
+  ];
+  function nameGrouping(person) {
+    return person[0];
+  }
+  function sumValues(value) {
+    return [
+      value[0],
+      value[1].reduce(function (result, person) {
+        return result + person[1];
+      }, 0)
+    ];
+  }
+  function naturalCompare(value1, value2) {
+    if (value1 < value2) {
+      return -1;
+    } else if (value1 > value2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 
-describe('join tests', () => {});
+  test('SELECT name, sum(value) FROM persons ORDER BY naturalCompare GROUP BY nameGrouping', () => {
+    expect(query().select(sumValues).from(persons).orderBy(naturalCompare).groupBy(nameGrouping).execute()).toEqual([
+      ['Anna', 4],
+      ['Michael', 10],
+      ['Peter', 10]
+    ]);
+  });
 
-test('from() supports multiple collections', () => {
+  const numbers = [1, 2, 1, 3, 5, 6, 1, 2, 5, 6];
+  function id(value) {
+    return value;
+  }
+  function frequency(group) {
+    return { value: group[0], frequency: group[1].length };
+  }
+
+  test('SELECT number, count(number) FROM numbers GROUP BY number', () => {
+    expect(query().select(frequency).from(numbers).groupBy(id).execute()).toEqual([
+      { value: 1, frequency: 3 },
+      { value: 2, frequency: 2 },
+      { value: 3, frequency: 1 },
+      { value: 5, frequency: 2 },
+      { value: 6, frequency: 2 }
+    ]);
+  });
+
+  function greatThan1(group) {
+    return group[1].length > 1;
+  }
+  function isPair(group) {
+    return group[0] % 2 === 0;
+  }
+
+  test('SELECT number, count(number) FROM numbers GROUP BY number HAVING count(number) > 1 AND isPair(number)', () => {
+    expect(query().select(frequency).from(numbers).groupBy(id).having(greatThan1).having(isPair).execute()).toEqual([
+      { value: 2, frequency: 2 },
+      { value: 6, frequency: 2 }
+    ]);
+  });
+});
+
+describe('join tests', () => {
   const teachers = [
     { teacherId: '1', teacherName: 'Peter' },
     { teacherId: '2', teacherName: 'Anna' }
@@ -414,72 +479,31 @@ test('from() supports multiple collections', () => {
   function student(join) {
     return { studentName: join[1].studentName, teacherName: join[0].teacherName };
   }
-  expect(query().select(student).from(teachers, students).where(teacherJoin).execute()).toEqual([
-    { studentName: 'Michael', teacherName: 'Peter' },
-    { studentName: 'Rose', teacherName: 'Anna' }
-  ]);
-});
+  function tutor1(join) {
+    return join[1].tutor === '1';
+  }
 
-describe('where() and having() admit multiple AND and OR filters', () => {
-  test('where() multiple AND', () => {
-    const teachers = [
-      { teacherId: '1', teacherName: 'Peter' },
-      { teacherId: '2', teacherName: 'Anna' }
-    ];
-    const students = [
-      { studentName: 'Michael', tutor: '1' },
-      { studentName: 'Rose', tutor: '2' }
-    ];
-    function teacherJoin(join) {
-      return join[0].teacherId === join[1].tutor;
-    }
-    function student(join) {
-      return { studentName: join[1].studentName, teacherName: join[0].teacherName };
-    }
-    function tutor1(join) {
-      return join[1].tutor === '1';
-    }
+  test('from() supports multiple collections with join condition in where', () => {
+    expect(query().select(student).from(teachers, students).where(teacherJoin).execute()).toEqual([
+      { studentName: 'Michael', teacherName: 'Peter' },
+      { studentName: 'Rose', teacherName: 'Anna' }
+    ]);
+  });
+  test('from() supports multiple collections with join condition in where and other where condition', () => {
     expect(query().select(student).from(teachers, students).where(teacherJoin).where(tutor1).execute()).toEqual([
       { studentName: 'Michael', teacherName: 'Peter' }
     ]);
   });
-  test('where() multiple AND on numbers', () => {
-    const numbers = [1, 2, 3, 4, 5, 7];
-    function lessThan6(number) {
-      return number < 6;
-    }
-    function greaterThan2(number) {
-      return number > 2;
-    }
-    expect(query().select().from(numbers).where(lessThan6).where(greaterThan2).execute()).toEqual([3, 4, 5]);
-  });
-  test('where() multiple OR', () => {
-    const numbers = [1, 2, 3, 4, 5, 7];
-    function lessThan3(number) {
-      return number < 3;
-    }
-    function greaterThan4(number) {
-      return number > 4;
-    }
-    expect(query().select().from(numbers).where(lessThan3, greaterThan4).execute()).toEqual([1, 2, 5, 7]);
-  });
-  test('having() multiple AND', () => {
-    const numbers = [1, 2, 1, 3, 5, 6, 1, 2, 5, 6];
-    function greatThan1(group) {
-      return group[1].length > 1;
-    }
-    function isPair(group) {
-      return group[0] % 2 === 0;
-    }
-    function id(value) {
-      return value;
-    }
-    function frequency(group) {
-      return { value: group[0], frequency: group[1].length };
-    }
-    expect(query().select(frequency).from(numbers).groupBy(id).having(greatThan1).having(isPair).execute()).toEqual([
-      { value: 2, frequency: 2 },
-      { value: 6, frequency: 2 }
+
+  const numbers1 = [1, 2];
+  const numbers2 = [4, 5];
+
+  test('from() supports multiple collections', () => {
+    expect(query().select().from(numbers1, numbers2).execute()).toEqual([
+      [1, 4],
+      [1, 5],
+      [2, 4],
+      [2, 5]
     ]);
   });
 });
